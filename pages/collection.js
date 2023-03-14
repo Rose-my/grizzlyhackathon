60bb374926a47db98cdad6780b54352f53e91df0
Irene Lee <86876293+juwonleedev@users.noreply.github.com>
2023-03-14 14:31:54 +0900

    header setting

--- pages/collection.js
+++ pages/collection.js
@@ -1,21 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Header from '../components/header';
 
 export default function Main () {
     return (
-    <>
-        <Header /><View style={styles.container}>
-            <Text>This is main page</Text>
-            <StatusBar style="auto" />
-
-        </View></>
+      <View style={styles.container}>
+        <Text>Collection Page</Text>
+        <StatusBar style="auto" />
+        
+      </View>
     );
   }
 
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-      backgroundColor: '#fff'
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
     },
 });
